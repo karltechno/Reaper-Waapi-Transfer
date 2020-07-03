6c906519fa5994ab858b6feeb09d97f49b3fa7e1
Karl Davis <karl@karltechno.com>
2020-07-03 23:00:59 +0100

    Tidy up python configure script

--- configure_project.py
+++ configure_project.py
@@ -8,15 +8,15 @@ import subprocess
 def copy_akautobahn(wwise_sdk_dir):
     # Copy AkAutobahn sources.
     path_to_akautobahn = os.path.join(wwise_sdk_dir, 'samples', 'WwiseAuthoringAPI', 'cpp', 'SampleClient', 'AkAutobahn')
-    dest_akautobahn = os.path.join(os.path.dirname(__file__), 'ext', 'AkAutobahn', 'AkAutobahn')
+    dest_akautobahn = os.path.join(sys.path[0], 'ext', 'AkAutobahn', 'AkAutobahn')
     print('Copying AkAutobahn from: {} to {}'.format(path_to_akautobahn, dest_akautobahn))
     
     # clean dest then copy.
-    shutil.rmtree(dest_akautobahn,)
+    shutil.rmtree(dest_akautobahn)
     shutil.copytree(path_to_akautobahn, dest_akautobahn)
 
 def cmake_run(generator, platform, build_dir, cmake_vars):
-    path_to_src = os.path.relpath(os.getcwd(), build_dir)
+    path_to_src = os.path.relpath(sys.path[0], build_dir)
     cmake_args = ['cmake', path_to_src, '-G', generator, '-A', platform]
 
     for k,v in cmake_vars.items():
@@ -36,13 +36,13 @@ def cmake_run(generator, platform, build_dir, cmake_vars):
 def main():
     # Find Wwise SDK dir.
     argparser = argparse.ArgumentParser(description='Create solution for reaper-waapi-transfer')
-    argparser.add_argument('-wwise_sdk_root', required=False, help='Path to Wwise sdk, uses environment var by default')
-    argparser.add_argument('-cmake_generator', help='CMake Generator.', default='Visual Studio 15 2017')
-    argparser.add_argument('-cmake_generator_platform', help='CMake generator platform', default='x64')
-    argparser.add_argument('-build_dir', help='Path to generate solution', default='build')
+    argparser.add_argument('-wwise_sdk_root', required=False, help='Path to Wwise sdk (default: environment var {WWISESDK})')
+    argparser.add_argument('-cmake_generator', help='CMake Generator (default: Visual Studio 15 2017).', default='Visual Studio 15 2017')
+    argparser.add_argument('-cmake_generator_platform', help='CMake generator platform (default: x64).', default='x64')
+    argparser.add_argument('-build_dir', help='Path to generate CMake solution into (default: build/).', default='build')
 
-    argparser.add_argument('-reaper32_dir', help='Path to reaper32 directory.', required=False)
-    argparser.add_argument('-reaper64_dir', help='Path to reaper64 directory.', required=False)
+    argparser.add_argument('-reaper32_dir', help='Path to reaper32 directory (for debugging and automatically copying DLL).', required=False)
+    argparser.add_argument('-reaper64_dir', help='Path to reaper64 directory (for debugging and automatically copying DLL).', required=False)
 
     args = argparser.parse_args()
     
