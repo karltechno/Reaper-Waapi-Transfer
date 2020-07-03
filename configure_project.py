ae8f6c7b5f2d397747841361adcaace3f75c7c47
Karl Davis <karl@karltechno.com>
2020-07-03 22:25:00 +0100

    Python file to copy AkAutobahn + configure/generate CMake.

---
+++ configure_project.py
@@ -0,0 +1,74 @@
+from __future__ import print_function
+import argparse
+import sys
+import os
+import shutil
+import subprocess
+
+def copy_akautobahn(wwise_sdk_dir):
+    # Copy AkAutobahn sources.
+    path_to_akautobahn = os.path.join(wwise_sdk_dir, 'samples', 'WwiseAuthoringAPI', 'cpp', 'SampleClient', 'AkAutobahn')
+    dest_akautobahn = os.path.join(os.path.dirname(__file__), 'ext', 'AkAutobahn', 'AkAutobahn')
+    print('Copying AkAutobahn from: {} to {}'.format(path_to_akautobahn, dest_akautobahn))
+    
+    # clean dest then copy.
+    shutil.rmtree(dest_akautobahn,)
+    shutil.copytree(path_to_akautobahn, dest_akautobahn)
+
+def cmake_run(generator, platform, build_dir, cmake_vars):
+    path_to_src = os.path.relpath(os.getcwd(), build_dir)
+    cmake_args = ['cmake', path_to_src, '-G', generator, '-A', platform]
+
+    for k,v in cmake_vars.items():
+        cmake_args.append('-D')
+        cmake_args.append('{}={}'.format(k,v))
+
+    try:
+        os.makedirs(build_dir)
+    except FileExistsError:
+        pass
+
+    os.chdir(build_dir)
+    print('Running {}'.format(' '.join(cmake_args)))
+    subprocess.run(cmake_args)
+    
+
+def main():
+    # Find Wwise SDK dir.
+    argparser = argparse.ArgumentParser(description='Create solution for reaper-waapi-transfer')
+    argparser.add_argument('-wwise_sdk_root', required=False, help='Path to Wwise sdk, uses environment var by default')
+    argparser.add_argument('-cmake_generator', help='CMake Generator.', default='Visual Studio 15 2017')
+    argparser.add_argument('-cmake_generator_platform', help='CMake generator platform', default='x64')
+    argparser.add_argument('-build_dir', help='Path to generate solution', default='build')
+
+    argparser.add_argument('-reaper32_dir', help='Path to reaper32 directory.', required=False)
+    argparser.add_argument('-reaper64_dir', help='Path to reaper64 directory.', required=False)
+
+    args = argparser.parse_args()
+    
+    wwise_sdk_dir = args.wwise_sdk_root if args.wwise_sdk_root else os.getenv('WWISESDK')
+    if not wwise_sdk_dir:
+        print("WWISESDK is not set.")
+        return 1
+
+    if not os.path.isdir(wwise_sdk_dir):
+        print('Wwise SDK path does not exist: {}'.format(wwise_sdk_dir))
+        return 1
+
+    print('Using Wwise SDK Dir: {}'.format(wwise_sdk_dir))
+
+    copy_akautobahn(wwise_sdk_dir)
+    cmake_vars = dict()
+
+    cmake_vars['AKSDK_DIR'] = wwise_sdk_dir
+
+    if args.reaper32_dir:
+        cmake_vars['REAPER32_PATH'] = args.reaper32_dir
+
+    if args.reaper64_dir:
+        cmake_vars['REAPER64_PATH'] = args.reaper64_dir
+
+    cmake_run(args.cmake_generator, args.cmake_generator_platform, args.build_dir, cmake_vars)
+
+if __name__ == "__main__":
+    sys.exit(main())
